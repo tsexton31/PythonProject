f097d6e9ec5340099d4776dffa452f749a417b36
tsexton31 <tsexton31@tamu.edu>
2020-09-16 18:56:05 -0500

    Added a list of all dates and outputting total requests

--- pythonproject.py
+++ pythonproject.py
@@ -1,18 +1,31 @@
 import urllib.request
-import csv
 import os.path
 from os import path
 
-if path.isfile('log.csv') == False:
+dates = []
+
+if path.isfile('log.txt') == False:
 
 	print('Beginning file download with urllib...')
 	url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-	urllib.request.urlretrieve (url, "log.csv")
+	urllib.request.urlretrieve (url, "log.txt")
 	print('Download finished')
 
 else:
 	print('You already have the log file.')
 
 
+
+logfile = open('log.txt', 'r')
+
+for row in logfile:
+	splitrow = row.split(' ')
+	dates.append(splitrow[3]) #dates is a list of every date
+
+
+print(dates)
+
+#total number of rows
+print(f"Total requests in time period of log is {len(dates)} ")
 #log = open('ApacheLog.csv', 'w')
 #print('https://s3.amazonaws.com/tcmg476/http_access_log')
