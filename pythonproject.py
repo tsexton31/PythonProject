94d5d8cb6ff9667c245a72269e86a718346ab972
tsexton31 <tsexton31@tamu.edu>
2020-09-17 21:13:58 -0500

    Finished Product, the last 365 days is the whole log..

--- pythonproject.py
+++ pythonproject.py
@@ -4,7 +4,9 @@ from os import path
 
 dates = [] #list of all dates in raw format
 dates_clean = [] #list of all dates in day/month/year format
-dates_clean_ints = [] 
+last365days = []
+oct_ints = []
+entriesIn1995 = []
 
 if path.isfile('log.txt') == False:
 
@@ -27,10 +29,25 @@ for row in logfile:
 for date in dates:
 	dates_clean.append(date[1:12])
 
+for date in dates_clean:
 
+	if(date[7:] == '1995'):
+		last365days.append(date)
+		entriesIn1995.append(date)
+	elif(date[7:] == '1994' and (date [3:6] == 'Nov' or 'Dec')):
+		last365days.append(date)
+	elif(date[7:] == '1994' and (date[3:6] == 'Oct')): #this is a list of all the ints past the 10th day in october
+		oct_ints.append(int(date[:2]))
 
-print(dates_clean)
+for integer in oct_ints:
+	if integer >= 11:
+		integer.append(last365days)
 
+print()
+print(f"Total requests in 1995 is {len(entriesIn1995)}")
+print()
+print(f"Total requests in last 365 days is {len(last365days)}") #just realized the last 365 days is the whole log
 #total number of rows
-print(f"Total requests in time period of log is {len(dates)} ")
+print(f"Total requests in time period of log is {len(dates)}")
+
 #ends October 11 1995
